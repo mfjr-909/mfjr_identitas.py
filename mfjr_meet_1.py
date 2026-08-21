5dd49b8c5e3bef8dcd15cad73e1b3a2d9de670a8
mfjr-909 <muhfadhiljr2@gmail.com>
2026-08-21 14:29:17 +0700

    Add files via upload

---
+++ mfjr_meet_1.py
@@ -0,0 +1,5 @@
+#print:untuk menampilkan suatu baris
+print('Nama\t: Muhammad Fadhillah Januar Rohman\nNIM\t: 2605060053\nTTL\t: 20 Januari 2009\nGender\t: Laki-laki\nHobi\t: Bersepeda, Bulu Tangkis, Menggambar')
+
+
+      
