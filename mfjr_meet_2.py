6bcb898fd93ef0ea509f065d6d50ba2e311a2e73
mfjr-909 <muhfadhiljr2@gmail.com>
2026-08-28 14:55:14 +0700

    Add files via upload

---
+++ mfjr_meet_2.py
@@ -0,0 +1,52 @@
+print("hello dunia world")
+
+A = 1045681
+x = 25
+panjang = 200
+print('A')
+print('x')
+print('panjang')
+
+nilai_y = 450
+juta20 = 20000000
+nializ = 20.56
+print("nilai y =", nilai_y)
+print("nilai juta20 =", juta20)
+print("niali z =", nializ)
+a = 8
+print("nilai a =", a)
+b = a
+print("nilai b =", b)
+
+B = 30
+print("data B =", B)
+
+data_int = 50
+print("data : ", data_int)
+data_float = 74,5
+print("data : ", data_float)
+data_bool = False
+print("- data : ", data_bool)
+data_complex = (6,7,8)
+print("- data : ", data_complex)
+data_c_double = (12.9)
+print("- data : ", data_c_double)
+
+data_int = 36
+print(float(data_int))
+print(str(data_int))
+print(bool(data_int))
+data_float = 33.9
+print(int(data_float))
+print(str(data_float))
+print(bool(data_float))
+data_str = "17"
+print(float(data_str))
+print(int(data_str))
+print(bool(data_str))
+
+data = input("hallo akuh")
+print(data)
+
+data = int(input("masukkan angka: 123"))
+print('data')
