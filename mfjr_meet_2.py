2e0d17ecd7d57a8be1d49c962dc0da03d302da30
mfjr-909 <muhfadhiljr2@gmail.com>
2026-09-04 14:58:21 +0700

    Add files via upload

--- mfjr_meet_2.py
+++ mfjr_meet_2.py
@@ -1,12 +1,10 @@
 print("hello dunia world")
-
 A = 1045681
 x = 25
 panjang = 200
 print('A')
 print('x')
 print('panjang')
-
 nilai_y = 450
 juta20 = 20000000
 nializ = 20.56
@@ -17,10 +15,8 @@ a = 8
 print("nilai a =", a)
 b = a
 print("nilai b =", b)
-
 B = 30
 print("data B =", B)
-
 data_int = 50
 print("data : ", data_int)
 data_float = 74,5
@@ -31,8 +27,6 @@ data_complex = (6,7,8)
 print("- data : ", data_complex)
 data_c_double = (12.9)
 print("- data : ", data_c_double)
-
-data_int = 36
 print(float(data_int))
 print(str(data_int))
 print(bool(data_int))
@@ -45,8 +39,54 @@ print(float(data_str))
 print(int(data_str))
 print(bool(data_str))
 
-data = input("hallo akuh")
-print(data)
+#tipe data: string
+data_string = "Muhammad Fadhillah Januar Rohman"
+print("Nama : ", data_string)
+print(type(data_string))
 
-data = int(input("masukkan angka: 123"))
-print('data')
+#tipe data integer
+data_integer = "17 Tahun"
+print("Umur : ", data_integer)
+print(type(data_integer))
+
+#tipe data float
+data_float ="74,8 kg"
+print("Berat Badan : ", data_float)
+print(type(data_float))
+
+#mengkonversi tipe data satu ke yang lain
+#integer ke float dan string
+print("integer ke float dan string")
+data_int = 89
+data_float = float(data_int)
+data_str = str(data_int)
+print(float(data_int))
+print(str(data_int))
+
+print(" ")
+
+#float ke integeger
+print("float ke integer")
+data_float = 45,67
+#data_int = int(data_float)
+#print(int(data_float))
+
+print(" ")
+
+#string ke integer
+print("string ke integer")
+data_string = 123
+data_integer = int(data_str)
+print(int(data_str))
+
+print(" ")
+
+#input data user
+print("menginput data user")
+data1 = str(input("Nama: "))
+data2 = int(input("Umur: "))
+data3 = float(input("Tinggi Badan: "))
+
+print("Nama Saya: ", data1, type(data1))
+print("Umur Saya: ", data2, type(data2))
+print("Tinggi Badan Saya: ", data3, type(data3))
